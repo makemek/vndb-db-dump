a835472042aabebf5acb34bb512abc36794a01ce
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2018-12-02 12:59:37 +0700

    fetch in batch

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ import fs from 'fs-extra'
 
 async function init() {
   const w1 = await Worker.build()
+  const START_ID = 1
+  const MAX_CHARACTER_ID = 77791
+  const MAX_RESULTS = 25
+  let characters = []
 
-  const MAX_CHARACTER_ID = 77760
-  const ids = times(MAX_CHARACTER_ID, id => id + 1)
-  const characters = []
-  for(let n = 0; n < ids.length; ++n) {
-    const id = ids[n]
+  for(let n = START_ID - 1; n < MAX_CHARACTER_ID; n += MAX_RESULTS) {
+    const fromId = n + 1
+    const toId = n + MAX_RESULTS
+    console.log(fromId, toId)
     const {
       data: { items }
-    } = await w1.executeWithRetry(character.byId, id)
-    const char = items[0]
-    characters.push(char)
+    } = await w1.executeWithRetry(
+      character.byArg,
+      {
+        results: MAX_RESULTS,
+        filters: `id >= ${fromId} and id <= ${toId}`,
+      },
+    )
+    characters = characters.concat(items)
+    console.log('write to file')
     await fs.outputJson('file.dump.json', characters)
   }
 }
--- src/task/character.js
+++ src/task/character.js
@@ -1,7 +1,12 @@
+const byArg = (client, args) => {
+  return client.character(args)
+}
+
 const byId = (client, id) => {
   return client.character({ filters: `id = ${id}`})
 }
 
 export default {
+  byArg,
   byId,
 }
